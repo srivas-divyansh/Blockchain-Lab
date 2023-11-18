569b7386af5a9756214463c13e5e3e81d5993a42
Divyansh <srivas.divyansh22@gmail.com>
2023-11-18 13:36:27 +0530

    Added the Linking file

---
+++ frontend/components/Linking.js
@@ -0,0 +1,47 @@
+const ether = require('ethers');
+import abi from "./contracts/MyNFT.json";
+
+function Linking() {
+    const [state, setState] = useState({
+        provider: null,
+        signer: null,
+        contract: null
+       });
+       const [account, setAccount] = useState("None");
+       useEffect(() => {
+         const connectWallet = async () => {
+          const contractAddress = "0x5BD3D0c828a05451B0C2306737324bD394BA5174";
+          const contractABI = abi.abi;
+          try {
+            const {ethereum} = window;
+            if (ethereum){
+              const account = await ethereum.request({
+                method:"eth_requestAccounts",
+              });
+  
+              window.ethereum.on("chainChanged", () => {
+                window.location.reload();
+              });
+  
+              window.ethereum.on("accountChanged", () => {
+                window.location.reload();
+              })
+            
+            const provider = new ethers.providers.Web3Provider(window.ethereum);
+            const signer = provider.getSigner();
+            const contract = new ethers.Contract(contractAddress, contractABI, signer);
+            setAccount(account);
+            setState({provider, signer, contract});
+            } else {
+              alert("Please install Metamask");
+            }
+          } catch(error){
+            console.log(error);
+          }
+          }
+        connectWallet();
+       }, []);
+       console.log(state);
+}
+
+export default Linking;
